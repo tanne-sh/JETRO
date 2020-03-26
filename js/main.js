16b6d3a14f5f2332784e8f623bfac5b2761330f6
Tatyana Shurinova <tatyana.shurinova@gmail.com>
2020-03-27 00:32:39 +0200

    fixed slider
    Create slider carousel__inner

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(function() {
-  $(".slider__main").slick({});
+  $(".slider__inner").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    infinite: true
+  });
+
+  $(".slider-carousel").slick({
+    lazyLoad: "ondemand",
+    slidesToShow: 6,
+    asNavFor: ".slider__inner",
+    dots: false,
+    arrows: false,
+    centerMode: true,
+    infinite: true,
+    focusOnSelect: true
+  });
 });
